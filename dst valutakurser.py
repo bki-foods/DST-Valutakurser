801abe15dcb5ade5781c4e6acd4c830af099e7b1
Nmobki <nmo@bki.dk>
2021-01-26 15:36:26 +0100

    Update dst valutakurser.py

--- dst valutakurser.py
+++ dst valutakurser.py
@@ -6,18 +6,19 @@ import urllib
 import pandas as pd
 from sqlalchemy import create_engine
 
-
-
 # Create variables
 now = datetime.datetime.now()
 scriptName = 'QM_Tea.py'
 executionId = int(now.timestamp())
 script_name = 'dst valutakurser.py'
+timestamp = datetime.datetime.now()
 
 params = urllib.parse.quote_plus('DRIVER={SQL Server Native Client 11.0};SERVER=sqlsrv04;DATABASE=BKI_Datastore;Trusted_Connection=yes')
 engine = create_engine('mssql+pyodbc:///?odbc_connect=%s' % params)
 
-
+# Function for insert into SQL database
+def insert_sql(dataframe, table_name, schema):
+    dataframe.to_sql(table_name, con=engine, schema=schema, if_exists='append', index=False)
 
 # =============================================================================
 # Get data from danmarks statistik. One query per currency. Time = 2000 - 2099
@@ -31,19 +32,15 @@ data = PyDST.get_data(table_id = 'DNVALD', variables = {'KURTYP':'KBH'
 df = PyDST.utils.to_dataframe(data)
 df['Valutakode'] = 'USD'
 df['ExecutionId'] = executionId
-
-print(df)
-
-
-# Function for insert into SQL database
-def insert_sql(dataframe, table_name, schema):
-    dataframe.to_sql(table_name, con=engine, schema=schema, if_exists='append', index=False)
-    
+df['Timestamp'] = timestamp
+# Remove non-numeric values (.. and 0 values)
+df = df[~df['INDHOLD'].isin(['..','0.0000'])]
+# Create dataframe for log insert    
 df_log = pd.DataFrame(data= {'Date':now, 'Event':script_name, 'Note': 'USD from dst - execution id: ' + str(executionId)}, index=[0])    
     
 # =============================================================================
 # Insert into currency table as well as dbo.log
 # =============================================================================
         
-
+insert_sql(df ,'Valutakurser' ,'fin')
 insert_sql(df_log, 'Log', 'dbo')
